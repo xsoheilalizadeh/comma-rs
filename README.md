7d53806e3ff10b37c5e455cd9e5304286b562068
xsoheilalizadeh <xsoheilalizadeh@gmail.com>
2020-07-03 17:50:50 +0430

    Inital Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## comma-rs
+
+Someday I needed to separate a long list of ticket numbers with ',' to use them for a SQL query within a `IN` rang condition, so I wrote this to avoid manual operation.    
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use clipboard::*;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let file = File::open("items.txt").unwrap();
+
+    let lines = BufReader::new(file).lines();
+
+    let mut lines_count= 0;
+
+    let mut output = String::new();
+
+    for line in lines {
+        lines_count = lines_count + 1;
+
+        let mut item = String::from(line.unwrap());
+
+        item.push_str(",\r\n");
+
+        output.push_str(item.as_str())
+    }
+
+    let mut clipboard: ClipboardContext = ClipboardProvider::new().unwrap();
+
+    clipboard.set_contents(output).unwrap();
+
+    println!("{} items has copied to clipboard!", lines_count)
+}
